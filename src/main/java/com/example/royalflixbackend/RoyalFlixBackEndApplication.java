1e5587ac904071bfd14679f4a815b7338da4b424
royalnavghans <93833578+royalnavghans@users.noreply.github.com>
2023-04-28 22:31:28 +0530

    init

---
+++ src/main/java/com/example/royalflixbackend/RoyalFlixBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.royalflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalFlixBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoyalFlixBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/royalflixbackend/RoyalFlixBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.royalflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalFlixBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
