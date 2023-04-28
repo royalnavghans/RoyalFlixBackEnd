105109ec02e467de9335d30e3ed03afabf0d47e1
royalnavghans <93833578+royalnavghans@users.noreply.github.com>
2023-04-28 23:17:27 +0530

    init

--- src/main/java/com/example/royalflixbackend/repo/mongoRepo.java
+++ src/main/java/com/example/royalflixbackend/repo/mongoRepo.java
@@ -2,5 +2,5 @@ package com.example.royalflixbackend.repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface mongoRepo extends MongoRepository {
+public interface mongoRepo  {
 }
